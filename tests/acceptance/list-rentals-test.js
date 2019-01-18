d5aa102b0c62d75cbfc87cb8bf4f4c9050e16830
최동호 <killi8n@okcoinkr.com>
2019-01-18 11:08:18 +0900

    commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,38 @@
+import { module, test } from "qunit";
+import { click, currentURL, visit } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
+
+module("Acceptance | list rentals", function(hooks) {
+  setupApplicationTest(hooks);
+
+  test("should show rentals as the home page", async assert => {
+    await visit("/");
+    await click(".menu-about");
+    assert.equal(currentURL(), "/about", "should navigate to about page");
+  });
+
+  test("should link to contact information", async assert => {
+    await visit("/");
+    await click(".menu-contact");
+    assert.equal(currentURL(), "/contact", "should navigate to contact page");
+  });
+
+  test("should list available rentals", async function(assert) {
+    await visit("/");
+    assert.equal(
+      this.element.querySelectorAll(".listing").length,
+      3,
+      "should display 3 listings"
+    );
+  });
+
+  // test("should link to information about the company.", async function(assert) {});
+
+  // test("should link to contact information.", async function(assert) {});
+
+  // test("should list available rentals.", async function(assert) {});
+
+  // test("should filter the list of rentals by city.", async function(assert) {});
+
+  // test("should show details for a selected rental", async function(assert) {});
+});
